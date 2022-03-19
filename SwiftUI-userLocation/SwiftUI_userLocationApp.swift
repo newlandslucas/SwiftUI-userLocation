707764a0c29d13467638597effb35441b023b076
newlandslucas <lucasnewlands.m@gmail.com>
2022-03-19 16:21:50 -0300

    feat: Start Project

---
+++ SwiftUI-userLocation/SwiftUI_userLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_userLocationApp.swift
+//  SwiftUI-userLocation
+//
+//  Created by Lucas Newlands on 19/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_userLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
